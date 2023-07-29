ed866567c08ba9739768a1ea8c355941f7bc0ffe
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-07-29 18:16:45 +0930

    add sorted by date

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -24,34 +24,36 @@ function Home() {
 
   const userName = auth.currentUser?.displayName?.split(" ")[0];
 
-  expensesData?.filter((item) => context?.userId === item.uid);
-
+  const filteredExpensesByUser = expensesData?.filter(
+    (item) => context?.userId === item.uid,
+  );
+  const sortedExpensesByDate = filteredExpensesByUser.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
+  );
   return (
     <div className="p-3 bg-blue-300 w-full">
       <ErrorMessage errorMsg={errorMsg} setErrorMsg={setErrorMsg} />
       <div className=" ">
         <p>Welcome back {userName}, </p>
         <div className="flex flex-col gap-2">
-          {expensesData
-            ?.filter((item) => context?.userId === item.uid)
-            .map((item) => (
-              <div
-                className="flex gap-2 border-2 bg-gray-200 rounded-md items-center justify-left p-2 text-2xl"
-                id={crypto.randomUUID()}
-              >
-                <span className="text-5xl">
-                  {
-                    categories[
-                      item.category.toLowerCase() as keyof typeof categories
-                    ]
-                  }
-                </span>
-                <h2>{item.item}</h2>
-                <p>{item.description}</p>
-                <p>{item.date === getDate() ? "Today" : item.date}</p>
-                <p>${item.cost}</p>
-              </div>
-            ))}
+          {sortedExpensesByDate.map((item) => (
+            <div
+              className="flex gap-2 border-2 bg-gray-200 rounded-md items-center justify-left p-2 text-2xl"
+              id={crypto.randomUUID()}
+            >
+              <span className="text-5xl">
+                {
+                  categories[
+                    item.category.toLowerCase() as keyof typeof categories
+                  ]
+                }
+              </span>
+              <h2>{item.item}</h2>
+              <p>{item.description}</p>
+              <p>{item.date === getDate() ? "Today" : item.date}</p>
+              <p>${item.cost}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
