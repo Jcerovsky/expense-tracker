67420683e538a4feb72c6b0300cfa178678ba00f
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-08-01 14:11:42 +0930

    change date to yesterday if applicable

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ import { auth, deleteData } from "../utils/firebase";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../context/UserContext";
 import { ErrorMessage } from "./ErrorMessage";
-import { getDate } from "../utils/getDate";
+import { getDate, isYesterday } from "../utils/getDate";
 import { categories } from "../utils/categories";
 import { formatNumber } from "../utils/formatNumberToIncludeDecimalPlaces";
 
@@ -50,7 +50,7 @@ function Home() {
     <div className="p-3 bg-blue-300 w-full">
       <ErrorMessage errorMsg={errorMsg} setErrorMsg={setErrorMsg} />
       <div>
-        <div className="bg-teal-300 p-1 pl-4 pr-4 rounded-2xl inline-flex flex-col gap-1 items-center justify-center ">
+        <div className="bg-teal-300 p-1 pl-4 pr-4 text-xs rounded-2xl inline-flex gap-1 items-center justify-center mb-3">
           <p>Spending today</p>
           <p className="mr-auto">${spendingToday}</p>
         </div>
@@ -72,7 +72,11 @@ function Home() {
                 <p>{item.description}</p>
               </div>
               <p className="absolute top-0 left-0.5 text-xs">
-                {item.date === getDate() ? "Today" : item.date}
+                {item.date === getDate()
+                  ? "Today"
+                  : item.date === isYesterday()
+                  ? "Yesterday"
+                  : item.date}
               </p>
               <p className="text-xl bold ml-auto">${formatNumber(item.cost)}</p>
               <div
--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -4,3 +4,18 @@ export const getDate = () => {
   const date = String(new Date().getDate()).padStart(2, "0");
   return `${year}-${month}-${date}`;
 };
+
+export const isYesterday = () => {
+  const today = new Date();
+  const yesterday = new Date();
+  yesterday.setDate(today.getDate() - 1);
+
+  if (today.getDate() === 1) {
+    yesterday.setMonth(today.getMonth(), 0);
+  }
+  return `${String(yesterday.getFullYear())}-${String(
+    yesterday.getMonth() + 1,
+  ).padStart(2, "0")}-${String(yesterday.getDate()).padStart(2, "0")}`;
+};
+
+console.log(isYesterday());
