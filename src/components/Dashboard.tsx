e5d8be7fb3d9b2d498e43d63cd6a41eba68e6beb
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-08-02 14:13:56 +0930

    start Dashboard component funcitonality with a bug

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,18 +1,51 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { UserContext } from "../context/UserContext";
 import { calculateDateFromTimeframe } from "../utils/calculateDateFromTimeframe";
+import ExpenseIncomeItem from "./ExpenseIncomeItem";
 
 function Dashboard() {
   const context = useContext(UserContext);
+  const inputRefFrom = useRef<HTMLInputElement>(null);
+  const inputRefTo = useRef<HTMLInputElement>(null);
 
-  console.log(
-    "date",
-    context?.filteredByUser.filter(
-      (item) => item.date < calculateDateFromTimeframe("week"),
-    ),
+  const filteredByDateInput = context?.filteredByUser.filter(
+    (item) => item.date > calculateDateFromTimeframe("week"),
   );
 
-  return <div>{context?.filteredByUser.reduce((a, b) => a + b.cost, 0)}</div>;
+  const filteredByCustomUserInput = context?.filteredByUser.filter(
+    (item) =>
+      item.date >= inputRefFrom.current?.value &&
+      item.date <= inputRefTo.current?.value,
+  );
+
+  return (
+    <div>
+      <div>
+        <p>Display expenses and income</p>
+        <label htmlFor="">From</label>
+        <input type="date" ref={inputRefFrom} />
+        <label htmlFor="">To</label>
+        <input type="date" ref={inputRefTo} />
+      </div>
+      <div>
+        <p>Total spending from </p>
+        <p
+          onClick={() => {
+            console.log("clicked");
+            console.log(filteredByCustomUserInput);
+          }}
+        >
+          ${filteredByDateInput!.reduce((a, b) => a + b.cost, 0)}
+        </p>
+
+        {filteredByCustomUserInput!.length === 0
+          ? "Zero expenses or income found."
+          : filteredByDateInput!.map((item) => (
+              <ExpenseIncomeItem item={item} />
+            ))}
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
